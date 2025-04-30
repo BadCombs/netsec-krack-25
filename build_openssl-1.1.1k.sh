6aec7bb1871319bc6177a69ec049ee1dc3b6d0d4
Giovanni <gpettinacci5@gmail.com>
2025-04-30 08:24:10 -0400

    Put Build Openssl in a single file

--- build_openssl-1.1.1k.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-NETSEC_DIR=$PWD
-
-# Build openssl-1.1.1k
-cd ./openssl-1.1.1k
-./config --prefix=$NETSEC_DIR/opt/openssl-1.1.1k --openssldir=$NETSEC_DIR/opt/openssl-1.1.1k shared zlib && make && make install
--- build_openssl.sh
+++ build_openssl.sh
@@ -5,3 +5,7 @@ NETSEC_DIR=$PWD
 # Build openssl-1.0.2l
 cd ./openssl-1.0.2l
 ./config --prefix=$NETSEC_DIR/opt/openssl-1.0.2l --openssldir=$NETSEC_DIR/opt/openssl-1.0.2l shared zlib && make && make install
+
+# Build openssl-1.1.1k
+cd ./openssl-1.1.1k
+./config --prefix=$NETSEC_DIR/opt/openssl-1.1.1k --openssldir=$NETSEC_DIR/opt/openssl-1.1.1k shared zlib && make && make install
